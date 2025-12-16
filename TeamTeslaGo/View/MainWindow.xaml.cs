2b906245e0282235882165d2249a00463293d113
Yang-Seo-Ha <hahayhaha55@gmail.com>
2025-12-16 14:58:10 +0900

    Feat 임시 기능들 추가

--- TeamTeslaGo/View/MainWindow.xaml.cs
+++ TeamTeslaGo/View/MainWindow.xaml.cs
@@ -20,5 +20,37 @@ namespace TeamTeslaGo.View
         {
             InitializeComponent();
         }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: 시스템 시작 로직
+            TxtCurrentMode.Text = "RUNNING";
+            TxtSystemLog.Text += "\n> Start pressed.";
+        }
+
+        private void BtnSelectTarget_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: 타겟 선택 로직
+            TxtSystemLog.Text += "\n> Select target pressed.";
+        }
+
+        private void BtnChangeTarget_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: 타겟 변경 로직
+            TxtSystemLog.Text += "\n> Change target pressed.";
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: 시스템 종료 로직
+            TxtCurrentMode.Text = "IDLE";
+            TxtSystemLog.Text += "\n> Stop pressed.";
+        }
+
+        private void CameraView_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // TODO: 카메라 클릭(ROI 지정 등)
+            TxtSystemLog.Text += "\n> Camera clicked.";
+        }
     }
 }
